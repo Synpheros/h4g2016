f390d5b89eef82bf40f13181f84c8391c88c1cd0
gorco <antcal01@ucm.es>
2016-02-26 22:01:14 +0100

    New folders and minigame1

---
+++ Assets/_Scripts/Dream1/MiniGame1Face.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class MiniGame1Face : MonoBehaviour {
+
+	public float angle = 0;
+	public int secodsToComplete = 5;
+	public float radius = 5;
+	private float speed;
+
+	private Vector3 initPosition;
+	private Vector3 nextPosition;
+
+	private Animator animator;
+
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		this.initPosition = this.transform.localPosition;
+        speed = (2 * Mathf.PI) / secodsToComplete; //2*PI in degress is 360, so you get 5 seconds to complete a circle
+	}
+
+	void Update()
+	{
+		angle += speed * Time.deltaTime; //if you want to switch direction, use -= instead of +=
+		nextPosition.x = initPosition.x + Mathf.Cos(angle) * radius;
+		nextPosition.y = initPosition.y + Mathf.Sin(angle) * radius;
+
+		this.transform.localPosition = nextPosition;
+	}
+
+	public void Clicked()
+	{
+		DestroyObject(this.gameObject);
+	}
+
+	void OnMouseDown()
+	{
+		animator.SetTrigger("expl");
+		Invoke("Clicked", 1f);
+	}
+	
+}
---
+++ Assets/_Scripts/Dream1/MiniGame1Logic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniGame1Logic : MonoBehaviour {
+
+	private Transform canvasTransform;
+
+	void Start()
+	{
+		canvasTransform = GameObject.Find("Canvas").transform;
+
+	}
+
+	void Update()
+	{
+		if(canvasTransform.childCount == 0)
+		{
+			EndDream();
+        }
+	}
+
+	private void EndDream()
+	{
+		Debug.Log("End");
+	}
+}
