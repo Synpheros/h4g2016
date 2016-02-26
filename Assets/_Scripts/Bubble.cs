7393857e42e6a22ae7b5b7f86e05539e4f29167f
Iván J. Pérez <synpheros@hotmail.com>
2016-02-26 17:57:40 +0100

    Chat working

---
+++ Assets/_Scripts/Bubble.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Bubble : MonoBehaviour {
+
+    private enum BubbleState {STOP,MOVING};
+    private BubbleState state = BubbleState.STOP;
+
+    public string text;
+
+    private Vector2 finalPosition;
+
+	// Use this for initialization
+	void Start () {
+        resize ();
+
+        Debug.Log (this.GetComponent<RectTransform> ().rect.width);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        resize ();
+	}
+
+
+    public void moveTo(Vector2 position){
+        this.finalPosition = position;
+    }
+
+    public void resize(){
+        this.GetComponent<Text> ().text = this.text;
+        foreach (Transform t in transform) {
+            if (t.GetComponent<Text> () != null) {
+                t.GetComponent<Text> ().text = this.text;
+                break;
+            }
+        }
+    }
+
+
+
+    public float easing = 0.05f;
+    public Vector2 minXY;
+    public bool _____________________________;
+    // fields set dynamically
+    public float camZ; // The desired Z pos of the camera
+    void Awake() {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate () {
+        Vector3 destination;
+        // If there is no poi, return to P:[0,0,0]
+        if (finalPosition == null) {
+            destination = Vector3.zero;
+        } else {
+            destination = finalPosition;
+        }
+        // Limit the X & Y to minimum values
+        destination.y = Mathf.Max( minXY.y, destination.y );
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.localPosition, destination, easing);
+        // Retain a destination.z of camZ
+        destination.z = camZ;
+        // Set the camera to the destination
+        transform.localPosition = destination;
+    }
+}
---
+++ Assets/_Scripts/Chat.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class Chat : MonoBehaviour {
+
+    static public Chat S;
+
+    public GameObject receiveBubble;
+    public GameObject sendBubble;
+
+    private GameObject content;
+
+    private List<GameObject> bubbles;
+
+	// Use this for initialization
+	void Start () {
+        S = this;
+
+        this.bubbles = new List<GameObject> ();
+
+        this.content = GameObject.Find ("content");
+
+        /*sendMessage ("hola, que tal estas");
+        receiveMessage ("yo muy bien y tu");
+        sendMessage ("Genial, ¿Como te llamas?");
+        sendMessage ("... ¿Hola?");
+        receiveMessage ("Si, Estoy aqui");*/
+	}
+	
+	// Update is called once per frame
+    private float msg_time = 1f;
+    private float time_since_last_msg = 0;
+    bool a = false;
+	void Update () {
+        time_since_last_msg += Time.deltaTime;
+        if (time_since_last_msg > msg_time) {
+            time_since_last_msg = 0;
+            a = !a;
+            if (a) 
+                sendMessage ("hola, que tal estas");
+            else
+                receiveMessage ("bien y tu?");
+        }   
+	}
+
+    public void sendMessage(string text){
+        GameObject bubble = GameObject.Instantiate (sendBubble);
+        bubble.transform.SetParent (content.transform);
+
+
+        bubble.transform.localScale = new Vector3 (1, 1, 1);
+
+        bubble.GetComponent<Bubble> ().text = text;
+        bubble.GetComponent<Bubble> ().resize ();
+
+        bubble.transform.localPosition = new Vector3 (300 - (bubble.GetComponent<RectTransform> ().rect.width/2), 0, 0);
+
+        this.bubbles.Add (bubble);
+
+        this.pushBubbles (0f);
+    }
+
+    public void receiveMessage(string text){
+        GameObject bubble = GameObject.Instantiate (receiveBubble);
+        bubble.transform.SetParent (content.transform);
+
+
+        bubble.transform.localScale = new Vector3 (1, 1, 1);
+
+        bubble.GetComponent<Bubble> ().text = text;
+        bubble.GetComponent<Bubble> ().resize ();
+
+        bubble.transform.localPosition = new Vector3 (100  + (bubble.GetComponent<RectTransform> ().rect.width/2), 0, 0);
+
+        this.bubbles.Add (bubble);
+
+        this.pushBubbles (0f);
+    }
+
+    public void pushBubbles(float height){
+        foreach (GameObject go in bubbles) {
+            go.GetComponent<Bubble>().moveTo(new Vector3 (go.transform.localPosition.x, go.transform.localPosition.y + height + 35, go.transform.localPosition.z));
+        }
+    }
+}
