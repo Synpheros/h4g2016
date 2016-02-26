92ba8972b47d610b3273b603f7025c2d793fdb7b
Iván J. Pérez <synpheros@hotmail.com>
2016-02-26 15:35:13 +0100

    Test chat and scroll

---
+++ Assets/_Scripts/AutoResizer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoResizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float height = 0f;
+        foreach (Transform t in this.transform) {
+            if (t.GetComponent<Text> () != null) {
+                GUIStyle height_calculator = new GUIStyle();
+                GUIContent guiTextContent = new GUIContent(this.GetComponent<Text>().text);
+
+                float textWidth = this.GetComponent<Text>().preferredWidth;
+                height = height_calculator.CalcHeight(guiTextContent , textWidth) + 20;
+                break;
+            }
+        }
+        this.GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
