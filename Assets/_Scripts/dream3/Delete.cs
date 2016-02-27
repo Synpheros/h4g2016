2c7f3abc3e14f9dbd6374ce19adfd3fdc42a40e9
RotaruDan <drotaru@ucm.es>
2016-02-27 02:15:34 +0100

    Adds dream3 full implementation

---
+++ Assets/_Scripts/dream3/Delete.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Delete : MonoBehaviour, IPointerEnterHandler
+{
+    public UnityAction onDuplicate;
+    private Vector3 targetPosition;
+    private float speed = 100.0f;
+    private float threshold = 0.5f;
+    private int pointerEnterCount;
+
+    void Start()
+    {
+        pointerEnterCount = 0;
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            duplicate();
+        });
+    }
+
+    void duplicate()
+    {
+        if(onDuplicate != null)
+        {
+            onDuplicate();
+        }
+    }
+
+    void Update()
+    {
+        Vector3 direction = targetPosition - transform.localPosition;
+        if (direction.magnitude > threshold)
+        {
+            transform.localPosition = transform.localPosition + direction * speed * Time.deltaTime;
+        }
+        else
+        {
+            // Without this game object jumps around target and never settles
+            transform.localPosition = targetPosition;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        pointerEnterCount++;
+        if (pointerEnterCount == 5)
+        {
+            pointerEnterCount = 0;
+            duplicate();
+        }
+        targetPosition = new Vector3(-Screen.width * .45f + UnityEngine.Random.value * Screen.width * .9f,
+            -Screen.height * .45f + UnityEngine.Random.value * Screen.height * .9f, 0f);
+    }
+
+}
---
+++ Assets/_Scripts/dream3/Dream3.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Dream3 : MonoBehaviour
+{
+
+    public GameObject delButtonPrefab;
+    private Graphic background;
+    private float totalDreamTime, remainingDreamTime;
+
+    private GameObject dream3;
+    private int dupCount;
+
+    // Use this for initialization
+    void Start()
+    {
+        dupCount = 0;
+        totalDreamTime = 15f;
+        remainingDreamTime = totalDreamTime;
+        dream3 = GameObject.Find("Dream3");
+        background = dream3.transform.FindChild("D3 Background").GetComponent<Graphic>();
+        background.CrossFadeColor(new Color(0f, 0f, 0f, 1f), totalDreamTime, false, false);
+        initDeleteButton();
+    }
+
+    void Update()
+    {
+        remainingDreamTime -= Time.deltaTime;
+
+        if (remainingDreamTime < 0f)
+        {
+            wakeUp();
+        }
+    }
+
+    void instantiateDeleteButton()
+    {
+        GameObject paperBall = PrefabUtils.LoadPrefab(dream3.transform, delButtonPrefab);
+
+        Delete paperballComp = paperBall.GetComponent<Delete>();
+        paperballComp.onDuplicate = () =>
+        {
+            Shake camShake = dream3.GetComponent<Shake>();
+            if (!camShake.Shaking)
+            {
+                camShake.DoShake();
+            }
+
+            dupCount++;
+
+            if (dupCount == 10)
+            {
+                wakeUp();
+                return;
+            }
+            initDeleteButton();
+        };
+
+        paperBall.transform.localPosition = new Vector3(-Screen.width * .3f + Random.value * Screen.width * .6f,
+            -Screen.height * .3f + Random.value * Screen.height * .6f, 0f);
+
+        Graphic graphic = paperBall.GetComponent<Graphic>();
+        float rgb = remainingDreamTime / totalDreamTime;
+        graphic.color = new Color(rgb, rgb, rgb, 1f);
+        graphic.CrossFadeAlpha(0f, 0f, false);
+        graphic.CrossFadeAlpha(1f, Random.value * 0.25f, false);
+      
+    }
+
+    void initDeleteButton()
+    {
+        if (Random.value > 0.5f)
+        {
+            instantiateDeleteButton();
+        }
+        instantiateDeleteButton();
+    }
+
+    void wakeUp()
+    {
+        // TODO Wake up and go to Day 3
+
+    }
+}
