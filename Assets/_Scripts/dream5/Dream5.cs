19cd7c305a619b9e72d934a9a089775a540c7af6
RotaruDan <drotaru@ucm.es>
2016-02-27 06:07:42 +0100

    Adds dream5 minigame

---
+++ Assets/_Scripts/dream5/Dream5.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Dream5 : MonoBehaviour {
+
+    private float totalDreamTime, remainingDreamTime;
+    // Use this for initialization
+    void Start ()
+    {
+        totalDreamTime = 15f;
+        remainingDreamTime = totalDreamTime;
+        PrefabUtils.IS_DREAM_5 = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        remainingDreamTime -= Time.deltaTime;
+
+        if (remainingDreamTime < 0f)
+        {
+            wakeUp();
+        }
+    }
+
+    void wakeUp()
+    {
+        SceneManager.LoadScene("transition");
+    }
+}
