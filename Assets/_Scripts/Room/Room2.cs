d87f66c5f70d91fcbf680c5824dcd1732c90df77
Iván J. Pérez <synpheros@hotmail.com>
2016-02-27 02:25:03 +0100

    Room 2 and 3 finished

--- Assets/_Scripts/Room/Room2.cs
+++ Assets/_Scripts/Room/Room2.cs
@@ -9,12 +9,10 @@ public class Room2 : MonoBehaviour {
         List<SequenceNode> sequence = new List<SequenceNode> ();
 
         sequence.Add(new SequenceNode("amistad", true, "Ventu",true));
-        sequence.Add(new SequenceNode("-Ha comentado en tu foto-", true, "Ventu"));
-        sequence.Add(new SequenceNode(" En esta foto das pena, jajajaja ", true, "Ventu"));
-        sequence.Add(new SequenceNode("-Has comentado en tu foto-", false));
-        sequence.Add(new SequenceNode(" XD ", false));
-        sequence.Add(new SequenceNode("-Ha comentado en tu foto-", true, "Ventu"));
-        sequence.Add(new SequenceNode(" Si, XD ", true, "Ventu"));
+        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\nEn esta foto das pena, jajajaja ", true, ""));
+        sequence.Add(new SequenceNode("-Has comentado en tu foto-\nXD ", false));
+        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\nSi, XD ", true, ""));
+        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\nYa verás mañana que risas", true, ""));
         sequence.Add(new SequenceNode("Mejor me voy a dormir...", false));
         sequence.Add (new SequenceNode ("Sleep"));
 
--- Assets/_Scripts/Room/Room3.cs
+++ Assets/_Scripts/Room/Room3.cs
@@ -8,29 +8,20 @@ public class Room3 : MonoBehaviour {
     void Start () {
         List<SequenceNode> sequence = new List<SequenceNode> ();
 
-        sequence.Add(new SequenceNode("-Ventu Ha comentado en tu foto-\n\t¡Que tonto eres!", true, ""));
-        sequence.Add(new SequenceNode("-Ventu Ha comentado en tu foto-\n\tQue tonto eres", true, ""));
-        sequence.Add(new SequenceNode("-Alicia Ha comentado en tu foto-\n\t¡Déjale en paz!", true, ""));
-        sequence.Add(new SequenceNode("-Ventu Ha comentado en tu foto-\n\tQue tonto eres", true, ""));
-        sequence.Add(new SequenceNode("-Juanma Ha comentado en tu foto-\n\tQue tonto eres", true, ""));
-        sequence.Add(new SequenceNode("-Paloma Ha comentado en tu foto-\n\tQue tonto eres", true, ""));
-        sequence.Add(new SequenceNode("-Valentín Ha comentado en tu foto-", true, ""));
-        sequence.Add(new SequenceNode("-Raúl Ha comentado en tu foto-", true, ""));
-        sequence.Add(new SequenceNode("-Marta Ha comentado en tu foto-", true, ""));
-        sequence.Add(new SequenceNode("-Antonio Ha comentado en tu foto-", true, ""));
-        sequence.Add(new SequenceNode("-Teresa Ha comentado en tu foto-", true, ""));
-        sequence.Add(new SequenceNode("-Iván Ha comentado en tu foto-", true, ""));
-        sequence.Add(new SequenceNode("-Has comentado en tu foto-", false));
-        sequence.Add(new SequenceNode("Por favor, ¡Parad ya!", false));
-        sequence.Add(new SequenceNode("-Miguel Ha comentado en tu foto-", true, "Miguel"));
-        sequence.Add(new SequenceNode("-Nerea Ha comentado en tu foto-", true, "Nerea"));
-        sequence.Add(new SequenceNode("-Juanma Ha comentado en tu foto-", true, "Juanma"));
-
-        sequence.Add(new SequenceNode("Mejor voy a borrar la foto...", false));
+        sequence.Add(new SequenceNode("-Ventu Ha comentado en tu foto-\n¡Que tonto eres!", true, ""));
+        sequence.Add(new SequenceNode("-Armando Ha comentado en tu foto-\nJajajaja, ¡Veeentu!", true, ""));
+        sequence.Add(new SequenceNode("-Ventu Ha comentado en tu foto-\nEres el mejor, no como este bobo.", true, ""));
+        sequence.Add(new SequenceNode("-Alicia Ha comentado en tu foto-\n¡Déjadle en paz!", true, ""));
+        sequence.Add(new SequenceNode("-Has comentado en tu foto-\nGracias :)", false));
+        sequence.Add(new SequenceNode("-Ventu Ha comentado en tu foto-\nTu calla, o irás después", true, ""));
+        sequence.Add(new SequenceNode("-Inés Ha comentado en tu foto-\nJajaja, Alicia perdedora.", true, ""));
+        sequence.Add(new SequenceNode("-Alicia Ha comentado en tu foto-\n...", true, ""));
+        sequence.Add(new SequenceNode("-Has comentado en tu foto-\nPor favor, ¡Parad ya!", false));
+        sequence.Add(new SequenceNode("-Alicia Ha comentado en tu foto-\nIntenta ignorarles... Es mejor...", true, ""));
+        sequence.Add(new SequenceNode("-Has comentado en tu foto-\nMejor voy a borrar la foto...", false));
         sequence.Add(new SequenceNode("borrar", true, "",true));
 
         sequence.Add(new SequenceNode("-Tu foto ha sido borrada-", true, "Sistema"));
-
 
         sequence.Add(new SequenceNode("Por fin se acabó la pesadilla...", false));
         sequence.Add (new SequenceNode ("Sleep"));
