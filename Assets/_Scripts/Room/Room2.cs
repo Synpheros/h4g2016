2a36c4d9285c8c0d3676c67d1dcb42d43017490f
Iván J. Pérez <synpheros@hotmail.com>
2016-02-27 01:51:43 +0100

    day 2 finished, day 3 almost

---
+++ Assets/_Scripts/Room/Room2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room2 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        List<SequenceNode> sequence = new List<SequenceNode> ();
+
+        sequence.Add(new SequenceNode("amistad", true, "Ventu",true));
+        sequence.Add(new SequenceNode("-Ha comentado en tu foto-", true, "Ventu"));
+        sequence.Add(new SequenceNode(" En esta foto das pena, jajajaja ", true, "Ventu"));
+        sequence.Add(new SequenceNode("-Has comentado en tu foto-", false));
+        sequence.Add(new SequenceNode(" XD ", false));
+        sequence.Add(new SequenceNode("-Ha comentado en tu foto-", true, "Ventu"));
+        sequence.Add(new SequenceNode(" Si, XD ", true, "Ventu"));
+        sequence.Add(new SequenceNode("Mejor me voy a dormir...", false));
+        sequence.Add (new SequenceNode ("Sleep"));
+
+        SequenceManager.S.current_sequence = sequence;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Assets/_Scripts/School3/School3.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class School3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        List<SequenceNode> sequence = new List<SequenceNode> ();
+
+		sequence.Add(new SequenceNode("Ventu: ¿Habéis visto la foto que comenté ayer?  ¡Das pena!", new int[] { -1, 0, -1 }));
+		sequence.Add(new SequenceNode("Tu: Si sigues así la pienso borrar...", new int[] { -1, -1, -1 }));
+        sequence.Add(new SequenceNode("Ventu: Borra lo que quieras, nada va a cambiar...", new int[] { -1, 3, -1 }));
+        sequence.Add(new SequenceNode("Ventu: ¡Mirad! ¡Mirad!... ¡Está en su perfil social!", new int[] { -1, 3, -1 }));
+        sequence.Add(new SequenceNode("Laughs"));
+		sequence.Add(new SequenceNode("Toda la clase: Risas!", new int[] { 11, 1, 31 }));
+		sequence.Add(new SequenceNode("Ventu: Y tú, Alicia, ten cuidado con quién te juntas, no vaya a pegarte su idiotez", new int[]{3,-1,1}));
+        sequence.Add(new SequenceNode("Alicia: ...", new int[]{3,-1,1}));
+        sequence.Add(new SequenceNode("Alicia: No te preocupes, yo estoy contigo...", new int[]{-1,21,-1}));
+
+
+		SequenceManager.S.current_sequence = sequence;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
