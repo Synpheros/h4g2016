426ce06cccf0e7ac6a8868e68cd5067f7318a7cb
RotaruDan <drotaru@ucm.es>
2016-02-27 15:13:01 +0100

    Fixes Day 5 dialog.

--- Assets/_Scripts/School5/School5.cs
+++ Assets/_Scripts/School5/School5.cs
@@ -14,8 +14,8 @@ public class School5 : MonoBehaviour
 		sequence.Add(new SequenceNode("Ventu: Sorpresa!!! A ver qué te parece la nueva decoración de la clase!", new int[] { -1, -1, 1 }));
 		sequence.Add(new SequenceNode("Armando: Si, te acuerdas de la imagen de anoche??? Esa que borraste.", new int[] { 30, -1, 1 }));
 		sequence.Add(new SequenceNode("Ventu: Jajaj, eres patético, díselo Alicia, dile lo triste que es.", new int[] { 0, -1, -1 }));
-		sequence.Add(new SequenceNode("Alicia: Eres un perdedor, no te molestes en hablarme más", new int[] { 0, -1, 20 }));
-		sequence.Add(new SequenceNode("Inés: No sé para qué vienes a clase, no te va a servir de nada", new int[] { 0, 10, 20 }));
+		sequence.Add(new SequenceNode("Alicia: Eres un perdedor, no te molestes en hablarme más", new int[] { 0, -1, 22 }));
+		sequence.Add(new SequenceNode("Inés: No sé para qué vienes a clase, no te va a servir de nada", new int[] { 0, 10, 22 }));
 		sequence.Add(new SequenceNode("Teacher"));
 		sequence.Add(new SequenceNode("Profesor: Buenos días, empecemos con la clase de hoy ", new int[] { -1, 40, -1 }));
 		sequence.Add(new SequenceNode("Position"));
