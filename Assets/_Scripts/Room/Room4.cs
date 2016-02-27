73ac2e26cd1b917e4e60efc4aacb33e98ee5a876
Iván J. Pérez <synpheros@hotmail.com>
2016-02-27 09:37:49 +0100

    THE END

--- Assets/_Scripts/Room/Room4.cs
+++ Assets/_Scripts/Room/Room4.cs
@@ -9,18 +9,18 @@ public class Room4 : MonoBehaviour {
         List<SequenceNode> sequence = new List<SequenceNode> ();
 
         sequence.Add(new SequenceNode("notificacion", true, "¡Te han etiquetado en una foto!",true));
-        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\n¡Jajaj, eso eso, etiquetadle en todas!", true, ""));
+        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\n¡Jajaj, eso eso, etiquetadle en todas!", true, "Ventu"));
         sequence.Add(new SequenceNode("-Has comentado en tu foto-\n¡OTRA VEZ! ¡¿POR QUE?!", false));
         sequence.Add(new SequenceNode("notificacion", true, "¡Te han etiquetado en una foto!",true));
         sequence.Add(new SequenceNode("notificacion", true, "¡Te han etiquetado en una foto!",true));
-        sequence.Add(new SequenceNode("-Alicia ha comentado en tu foto-\n¡Dejadle en paz!", true, ""));
-        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\n...Te lo advertí Alicia.", true, ""));
-        sequence.Add(new SequenceNode("-Valentín ha comentado en tu foto-\nAdemás de Fea, tonta.", true, ""));
-        sequence.Add(new SequenceNode("-Teresa ha comentado en tu foto-\nQue pasa ¿Es tu novio?", true, ""));
-        sequence.Add(new SequenceNode("-Raúl ha comentado en tu foto-\nEres tan inútil como el.", true, ""));
-        sequence.Add(new SequenceNode("-Miguel ha comentado en tu foto-\nEs que con esa cara jjaja", true, ""));
-        sequence.Add(new SequenceNode("-Nerea ha comentado en tu foto-\nMoriros ya.", true, ""));
-        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\nExactamente...", true, ""));
+        sequence.Add(new SequenceNode("-Alicia ha comentado en tu foto-\n¡Dejadle en paz!", true, "Alicia"));
+        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\n...Te lo advertí Alicia.", true, "Ventu"));
+        sequence.Add(new SequenceNode("-Valentín ha comentado en tu foto-\nAdemás de Fea, tonta.", true, "Valentín"));
+        sequence.Add(new SequenceNode("-Teresa ha comentado en tu foto-\nQue pasa ¿Es tu novio?", true, "Teresa"));
+        sequence.Add(new SequenceNode("-Raúl ha comentado en tu foto-\nEres tan inútil como el.", true, "Raúl"));
+        sequence.Add(new SequenceNode("-Miguel ha comentado en tu foto-\nEs que con esa cara jjaja", true, "Miguel"));
+        sequence.Add(new SequenceNode("-Nerea ha comentado en tu foto-\nMoriros ya.", true, "Nerea"));
+        sequence.Add(new SequenceNode("-Ventu ha comentado en tu foto-\nExactamente...", true, "Ventu"));
         sequence.Add(new SequenceNode("Alicia, No me dejes :(", false));
         sequence.Add(new SequenceNode("No se si podré aguantar...", true, "Alicia"));
         sequence.Add(new SequenceNode("Por favor... No me abandones...", false));
--- Assets/_Scripts/Room/Room5.cs
+++ Assets/_Scripts/Room/Room5.cs
@@ -8,13 +8,18 @@ public class Room5 : MonoBehaviour {
     void Start () {
         List<SequenceNode> sequence = new List<SequenceNode> ();
 
-		sequence.Add(new SequenceNode("Venga, me das pena, quitales 50€ a tu padres y \n traemelos mañana", true, "Ventu"));
-		sequence.Add(new SequenceNode("Entonces haré que todos borren las fotos y nos  \n olvidemos del tema", true, "Ventu"));
+		sequence.Add(new SequenceNode("Venga, me das pena, quitales 50€ a tu padres", true, "Ventu"));
+        sequence.Add(new SequenceNode("y traemelos mañana", true, "Ventu"));
+
+        sequence.Add(new SequenceNode("Entonces haré que todos borren las fotos", true, "Ventu"));
+        sequence.Add(new SequenceNode("y nos olvidemos del tema", true, "Ventu"));
+
 		sequence.Add(new SequenceNode("De acuerdo, te los llevaré mañana", false));
 		sequence.Add(new SequenceNode("Así me gusta, pero esto no termina aquí", true, "Ventu"));
 		sequence.Add(new SequenceNode("Estoy deseando verte el próximo día en clase", true, "Ventu"));
-		sequence.Add(new SequenceNode("tendrás que hacer más cosas para mi \n si no quieres que vuelva a compartir la foto", true, "Ventu"));
-		sequence.Add(new SequenceNode("O sino volveré a subir la foto", true, "Ventu"));
+        sequence.Add(new SequenceNode("tendrás que hacer más cosas para mi...", true, "Ventu"));
+        sequence.Add(new SequenceNode("si no quieres que vuelva a compartir la foto", true, "Ventu"));
+		sequence.Add(new SequenceNode("O sino la volveré a subir", true, "Ventu"));
 		sequence.Add(new SequenceNode ("Sleep"));
 
         SequenceManager.S.current_sequence = sequence;
