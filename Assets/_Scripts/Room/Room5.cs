589c8c854c1ee6a79797294465116670d3dbfafc
gorco <antcal01@ucm.es>
2016-02-27 07:57:02 +0100

    Added dream5.2 and day5

---
+++ Assets/_Scripts/Room/Room5.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room5 : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        List<SequenceNode> sequence = new List<SequenceNode> ();
+
+		sequence.Add(new SequenceNode("Venga, me das pena, quitales 50€ a tu padres y \n traemelos mañana", true, "Ventu"));
+		sequence.Add(new SequenceNode("Entonces haré que todos borren las fotos y nos  \n olvidemos del tema", true, "Ventu"));
+		sequence.Add(new SequenceNode("De acuerdo, te los llevaré mañana", false));
+		sequence.Add(new SequenceNode("Así me gusta, pero esto no termina aquí", true, "Ventu"));
+		sequence.Add(new SequenceNode("Estoy deseando verte el próximo día en clase", true, "Ventu"));
+		sequence.Add(new SequenceNode("tendrás que hacer más cosas para mi \n si no quieres que vuelva a compartir la foto", true, "Ventu"));
+		sequence.Add(new SequenceNode("O sino volveré a subir la foto", true, "Ventu"));
+		sequence.Add(new SequenceNode ("Sleep"));
+
+        SequenceManager.S.current_sequence = sequence;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
